3354578263c8d9ea42f5e9bf7c58809e6c6d86fe
Balde Saikou-oumar <balde@a12p8.fil.univ-lille1.fr>
2014-10-22 17:34:54 +0200

    Ajout fichier java

---
+++ tp.java
@@ -0,0 +1,42 @@
+package donnee;
+
+import java.util.*;
+
+
+public class Specialite {
+	
+	private String nom;
+	private HashMap<String,Patient> lespatients;
+	private HashMap<String,FicheSuivie> lesfichessuivies;
+	private Collection<Specialiste> lesspecialistes;
+	
+	public Specialite(String nom) {
+		this.nom = nom;
+		this.lespatients=new HashMap<String,Patient> ();
+		this.lesfichessuivies=new HashMap<String,FicheSuivie>();
+		this.lesspecialistes=new ArrayList<Specialiste> ();
+		
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public HashMap<String, Patient> getLespatients() {
+		return lespatients;
+	}
+
+	public HashMap<String, FicheSuivie> getLesfichessuivies() {
+		return lesfichessuivies;
+	}
+
+	public Collection<Specialiste> getLesspecialistes() {
+		return lesspecialistes;
+	}
+
+	
+}
